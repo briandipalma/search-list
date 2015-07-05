517e2bf8bee8365ace8499508259f776beab568c
Brian Di Palma <offler@gmail.com>
2015-07-05 13:22:18 +0100

    Display the results in capitals.

--- app/src/components/SearchResultGrid-react.js
+++ app/src/components/SearchResultGrid-react.js
@@ -8,29 +8,33 @@ import {branch} from "baobab-react/higher-order";
 
 class SearchResultGrid extends Component {
 	render() {
+		const cellRenderer = (cellData) => cellData.toUpperCase();
 		const rowGetter = (rowIndex) => this.props.currentSearchResults[rowIndex];
 
 		return <Table
 			rowHeight={50}
 			rowGetter={rowGetter}
 			rowsCount={this.props.currentSearchResults.length}
-			width={450}
+			width={550}
 			height={300}
 			headerHeight={50}>
 				<Column
 					dataKey="name"
 					label="Name"
 					width={100}
+					cellRenderer={cellRenderer}
 					/>
 				<Column
 					dataKey="superpower"
 					label="Superpower"
-					width={100}
+					width={150}
+					cellRenderer={cellRenderer}
 					/>
 				<Column
 					dataKey="end_of_an_era"
 					label="End of Era"
 					width={250}
+					cellRenderer={cellRenderer}
 					/>
 			</Table>;
 	}
